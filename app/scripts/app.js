b6a7aa2c5adcc850cb6044cdc7002b1283f8a1d7
IustiNN <iustin.nitza@gmail.com>
2015-03-31 15:23:07 +0300

    struggle with authentication state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,6 +22,15 @@
   'ngSanitize',
   'ngTouch'
   ])
+   .run(['$rootScope', '$location', function($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+      // We can catch the error thrown when the $requireAuth promise is rejected
+      // and redirect the user back to the home page
+      if (error === 'AUTH_REQUIRED') {
+        $location.path('/main');  
+      }
+    });
+}])
  .constant('FIREBASE_URL', 'https://dazzling-heat-502.firebaseio.com/')
  .config(function ($routeProvider) {
   $routeProvider
@@ -56,6 +65,20 @@
   .when('/login', {
     templateUrl: 'views/login.html',
     controller: 'AuthCtrl'
+  })
+  .when('/account', {
+    // the rest is the same for ui-router and ngRoute...
+    controller: 'AccountCtrl',
+    templateUrl: 'views/account.html',
+    resolve: {
+      // controller will not be loaded until $requireAuth resolves
+      // Auth refers to our $firebaseAuth wrapper in the example above
+      'currentAuth': ['Auth', function(Auth) {
+        // $requireAuth returns a promise so the resolve waits for it to complete
+        // If the promise is rejected, it will throw a $stateChangeError (see above)
+        return Auth.$requireAuth();
+      }]
+    }
   })
   .otherwise({
     redirectTo: '/'
