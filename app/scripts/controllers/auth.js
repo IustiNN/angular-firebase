edc669df0f9f99b88fee20af6f8dd77d6ab6b2c5
IustiNN <iustin.nitza@gmail.com>
2015-04-09 12:58:21 +0300

    Fixed the register and login

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -4,47 +4,48 @@ app.controller('AuthCtrl', ['$scope', 'Auth', '$location',
   function($scope, Auth, $location) {
       $scope.message = null;
       $scope.error = null;
-      // $scope.Auth = Auth;
-      // $scope.user = Auth.user;
-      $scope.authData = Auth.checkLogin();
-      // $scope.sharedValue = sharedProperties.getObject.status;
+      $scope.Auth = Auth;
+      $scope.user = Auth.user;
+
       if ($scope.user) {
         $location.path('/');
       }
+
   // any time auth status updates, add the user data to scope
-  //   Auth.$onAuth(function(authData) {
-  //     $scope.authData = authData;
-
-  // console.log(Auth.user);
-  //     console.log(authData);
-  //   });
-  
-    $scope.createUser = function() {
-      Auth.createUser($scope.user).then(function(userData) {
-      $scope.message = 'User created with uid: ' + userData.uid + 'and ' + userData.password.email;
-      console.log($scope.message);
-      // authenticate so we have permission to write to Firebase
-      // redirect to the account page
-      // $location.path('/posts');
-      // return Auth.loginUser($scope.user);
-
+    Auth.$onAuth(function(authData) {
+      $scope.authData = authData;
+      if (authData) {
+        console.log('Logged in as:', authData.uid);
+      } else {
+        console.log('Logged out');
+      }
     });
+
+  $scope.createUser = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth.$createUser({
+        email: $scope.email,
+        password: $scope.password
+      }).then(function(userData) {
+        $scope.message = 'User created with uid: ' + userData.uid;
+      }).catch(function(error) {
+        $scope.error = error;
+      });
     };
 
-    $scope.loginUser = function() {
-      Auth.loginUser($scope.user).then(function() {
-     console.log('Logged in as:'+ $scope.user.email  +' from login method');
-     console.log($scope.user);
-     $location.path('/posts');
-
-   }).catch(function(error) {
-     console.error('Authentication failed:', error);
-   });
+    $scope.login = function() {
+      Auth.$authWithPassword({
+        email: $scope.email,
+        password: $scope.password
+      },{rememberMe: true}).then(function(authData) {
+        console.log('Logged in as:', authData.uid);
+      }).catch(function(error) {
+        console.error('Authentication failed:', error);
+      });
     };
-
-    $scope.logoutUser = function() {
-      Auth.logoutUser();
-    };
+    
 
 
 
--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,64 +1,8 @@
 'use strict';
 app.factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
   var ref = new Firebase('https://dazzling-heat-502.firebaseio.com/');
-  var auth = $firebaseAuth(ref);
-  
-  var Auth = {
-    isLogged : false,
-    createUser : function(user) {
-      return auth.$createUser({
-        email: user.email,
-        password: user.pass
-      });
-    },
-
-    checkLogin : auth.$onAuth(function(authData) {
-    if (authData) {
-      console.log('Logged in as:', authData.uid);
-      Auth.isLogged = true;
-      return authData;
-    } else {
-      // this.isLogged = false;
-      console.log('Logged out');
-    }
-  }),
-
-    loginUser : function (user) {
-     return auth.$authWithPassword({
-     email: user.email,
-     password: user.password
-   },{rememberMe: true}).then(function() {
-      Auth.isLogged = true;
-   });
-  },
-
-  logoutUser : function () {
-    this.isLogged = false;
-   return auth.$unauth();
-   // if(Auth.user && Auth.user.profile) {
-   //   Auth.user.profile.$destroy();
-   // }
-   // angular.copy({}, Auth.user);
-  }, 
-
-    changePassword : function() {
-     return auth.$changePassword({
-       email: 'my@email.com',
-       oldPassword: 'mypassword',
-       newPassword: 'otherpassword'
-     }).then(function() {
-       console.log('Password changed successfully!');
-     }).catch(function(error) {
-       console.error('Error: ', error);
-     });
-   },
-   user: {}
-  };
-
-  return Auth;
-
-
-}]);
+  return  $firebaseAuth(ref);
+  }]);
 
 
 
