652f1407b6186908bc2a63e14fba494e7183c84a
Iustin Nita <iustin.nitza@gmail.com>
2015-07-08 02:29:07 +0300

    Add user profile

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -8,6 +8,15 @@ app.factory('User', function ($firebaseArray) {
   	create: function (user) {
       return users.$add(user);
     },
+    createProfile: function (user) {
+      var profile = {
+        username: user.username,
+        md5_hash: user.md5_hash
+      };
+
+      var profileRef = $firebase(ref.child('profile'));
+      return profileRef.$set(user.uid, profile);
+    },
     get: function (userId) {
     	console.log($firebaseArray(ref.child('users').child(userId)));
       return $firebaseArray(ref.child('users').child(userId));
